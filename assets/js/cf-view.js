39b7548a99380b98014fd0e5cb81b03a1a7ad6ad
Josh Pollock <jpollock412@gmail.com>
2015-08-19 01:42:36 -0400

    Make it work and documented. Needs: UI and FooTable includes

---
+++ assets/js/cf-view.js
@@ -0,0 +1,43 @@
+/**
+ * Created by josh on 8/18/15.
+ */
+jQuery( document ).ready( function ( $ ) {
+    var full_data = $( '#cf-view-full-parts' ).data( 'content' );
+
+    $( '.cf-view-shortened-view' ).on( 'click', function(e){
+        e.preventDefault();
+
+        var key =  $(this ).data( 'cf-shortened' );
+        if ( 'undefined' != full_data[ key ] ) {
+            $( '#cf-view-table' ).slideUp();
+            hide ( $( '#cf-view-table' ) );
+            hide( $( '.footable-filter' ) );
+
+            $( '#cf-view-full-viewer' ).html( full_data[ key ] );
+            show( $( '#cf-view-full-viewer' ) );
+            show( $( '#cf-full-close' ) );
+
+            $( '#cf-full-close' ).on( 'click', function(e) {
+                show ( $( '#cf-view-table' ) );
+                $( '#cf-view-table' ).slideDown();
+                show( $( '.footable-filter' ) );
+
+                $( '#cf-view-full-viewer' ).html( '' );
+                hide( $( '#cf-view-full-viewer' ) );
+                hide( $( '#cf-full-close' ) );
+            });
+
+
+
+        }
+
+    });
+
+    function hide( el ) {
+        el.css( 'visibility', 'hidden' ).attr( 'aria-hidden', 'true' );
+    }
+
+    function show( el ) {
+        el.css( 'visibility', 'visible' ).attr( 'aria-hidden', 'false' );
+    }
+} );
--- cf-view.php
+++ cf-view.php
@@ -6,8 +6,29 @@ if ( ! defined( 'CFCORE_PATH') ) {
 	return;
 }
 
+/**
+ * Load the JS.
+ *
+ * @todo include FooTable
+ *
+ * @since 0.0.1
+ */
+add_action( 'wp_enqueue_scripts', function() {
+	wp_register_script( 'cf-view', plugin_dir_url( __FILE__ ) .'/assets/js/cf-view.js', array( 'jquery', 'footable-min' ), false, true );
+});
 
-function cf_view( $form_id, $editor_id = null, $fields = array()  ) {
+/**
+ * Create CF View interface.
+ *
+ * @since 0.0.1
+ *
+ * @param int $form_id ID of form to view
+ * @param array $fields Optional. An array of fields to show. If empty, the default, all fields of form are shown.
+ * @param null|int $editor_id Optional. ID of a page with the form on it, used for editing. If null, the default, no edit links are shown.
+ *
+ * @return string|void
+ */
+function cf_view( $form_id, $fields = array(), $editor_id = null  ) {
 	if ( ! is_array( Caldera_Forms::get_form( $form_id ) ) ) {
 		return;
 	}
@@ -40,113 +61,13 @@ function cf_view( $form_id, $editor_id = null, $fields = array()  ) {
 			}
 		}
 
-		$class =  new cf_view( $fields, $entries, $editor_id );
+		include_once dirname( __FILE__ ) . '/classes/table.php';
+
+		wp_enqueue_script( 'cf-view' );
+		$class = new \calderawp\view\table( $fields, $entries, $editor_id );
+
 		return $class->get_markup();
 	}
+
 }
 
-
-
-class cf_view{
-
-	private $markup;
-
-	private $edit_mode;
-
-	private $shortened;
-
-	function __construct( $fields, $entries, $edit_id = null, $edit_permission = null ) {
-		if ( $edit_id ) {
-			$this->edit_mode = true;
-		}
-		$header = $this->header( $fields );
-		$rows = $this->rows( $entries, $fields, get_permalink( $edit_id ) );
-
-		if ( is_string( $header ) && is_string( $rows ) ) {
-			$this->make_table( $header, $rows );
-		}
-	}
-
-	public function get_markup() {
-		return $this->markup;
-	}
-
-	function make_table( $header, $rows ) {
-		$table = sprintf( '<table class="footable">%1s <tbody>%2s</tbody></table>', $header, $rows );
-		if ( ! empty( $this->shortened ) ) {
-			$table .= '<div id="cf-view-full-parts" data-content="' . esc_attr( wp_json_encode( $this->shortened ) ) . '"></div>';
-		}
-		$this->markup = $table;
-	}
-
-
-	function header( $fields ) {
-		$out[] = '<thead><tr>';
-		foreach( $fields as $field ) {
-			$id = $field[ 'ID' ];
-			$name = $field[ 'label' ];
-			$out[] = sprintf( '<th data-toggle="true" id="header-%1s">%2s</th>', esc_attr( $id ), $name );
-		}
-		if ( $this->edit_mode ) {
-			$out[] = sprintf( '<th data-toggle="false" id="header-edit">%2s</th>', __( 'Edit', 'cf-view' ) );
-		}
-		$out[] = '</tr></thead>';
-
-		return implode( '', $out );
-	}
-
-	function rows( $entries, $fields, $edit_link = false ) {
-
-
-		foreach( $entries as $entry ) {
-			$entry_id = $entry['_entry_id'];
-
-			$data = $entry[ 'data' ];
-			$out[] = sprintf( '<tr cf-view-entry-id="%1s" class="cf-view-entry-data">', esc_attr( $entry_id ) );
-
-			foreach( $fields  as $field ) {
-
-				$field_slug = $field[ 'slug' ];
-				$field_id = $field[ 'ID' ];
-				if ( isset( $data[ $field_slug ] ) ) {
-					$value = $data[ $field_slug ];
-					$out[] = $this->field_value_td( $value, $field_id, $entry_id );
-				}else{
-					$out[] = '<td></td>';
-				}
-
-
-
-
-			}
-
-			if ( $this->edit_mode ) {
-				$edit = add_query_arg( 'cf_id', (int) $entry_id, $edit_link );
-				$edit = sprintf( '<a href="%1s" class="cf-view-edit-link" cf-view-entry-id="%2s">%3s</a>', esc_url( $edit ), esc_attr( $entry_id ), __( 'Edit', 'cf-view' ) );
-				$out[] = sprintf( '<td>%1s</td>', $edit );
-			}
-
-			$out[] = '</tr>';
-
-		}
-
-		return implode( '', $out );
-
-	}
-
-	function field_value_td( $value, $field_id, $entry_id ) {
-		if ( 17 < strlen( $value ) ) {
-			$this->shortened[  $entry_id.$field_id ] = $value;
-			$value = sprintf( '<span class="cf-view-shortened-preview">%1s</span>',
-				substr( $value, 0, 17 ) . ' ' . sprintf( '<a href="#" class="cf-view-shortened-view" data-cf-shortened="%1s">%2s</a>', esc_attr( $entry_id . $field_id ), __( 'See All', 'cf-view' ) )
-			);
-
-		}
-		$html = sprintf( '<td cf-view-entry-id="%1s" cf-view-field-id="%2s">%3s</td>', esc_attr( $entry_id ), esc_attr( $field_id ), $value );
-		return $html;
-	}
-
-	function edit_td() {
-
-	}
-}
